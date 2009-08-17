b9edda12351a28455735a991af6afdfd3b017b46
Caged <justin@activereload.net>
2009-08-17 01:44:51 -0700

    Initial content scoring and parsing

--- lib/dragnet.rb
+++ lib/dragnet.rb
@@ -6,13 +6,13 @@ require 'dragnet/dragger'
 require 'open-uri'
 require 'pp'
 
-# Dir['/Users/justin/dev/me/ruby/sherlock/data/*.html'].each do |f|
-#   puts File.basename(f)
-#   puts Dragnet::Dragger.drag!(File.read(f))
-#   puts ""
-#   puts "-" * 100
-#   puts ""
-# end
+Dir['/Users/justin/dev/me/ruby/sherlock/data/*.html'].each do |f|
+  puts File.basename(f)
+  puts Dragnet::Dragger.drag!(File.read(f))
+  puts ""
+  puts "-" * 100
+  puts ""
+end
 
 #puts Dragnet::Dragger.drag!(File.read("/Users/justin/dev/me/ruby/sherlock/data/14cee27ee.html"))
 #puts Dragnet::Dragger.drag!(File.read("/Users/justin/dev/me/ruby/sherlock/data/0555370b0.html"))
